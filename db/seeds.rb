893dd520d9a119d222c07f026ec7c7d567c0626b
Marcel <marcelfonteles@gmail.com>
2019-05-23 02:20:34 +0000

    corrigin seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.all.each do |user|
+  user.destroy
+end
+Loan.all.each do |loan|
+  loan.destroy
+end
 User.create(email:'email@email.com', password:'password')
 3.times do 
   Loan.create(amount: 1000.00, firstPayment: '1994-12-05', name: "Marcel", store:"Loja 1", portion1: 250, portion2: 300, portion3: 240, date_p1: DateTime.now, date_p2: DateTime.now, date_p3: DateTime.now)
